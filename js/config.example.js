b922579ede1fe97844cb3db5b8ebffaaa0085671
GabrielHR0 <gabrielhenriqueojr@gmail.com>
2026-06-28 21:27:19 -0300

    feat: add config template and decision tree data

---
+++ js/config.example.js
@@ -0,0 +1,7 @@
+var APP_CONFIG = {
+  SITE_URL: 'https://SEU_DOMINIO_AQUI',
+  OG_IMAGE_URL: 'https://SEU_DOMINIO_AQUI/images/capa.png',
+  APP_NAME: 'GuiaCar',
+  APP_DESC: 'Guia do Cadastro Ambiental Rural',
+  USER_AGENT: 'GuiaCar/1.0'
+};
